a2c1938ca1b88d72b8d4b2cdf34456e33f6ee2e6
olivier dijkstra <olivierdijkstra@gmail.com>
2025-08-07 11:48:19 +0200

    statusline

---
+++ .config/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "auto",
+        component_separators = "",
+        section_separators = "",
+        icons_enabled = false,
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = { { "mode", fmt = function(str) return str:sub(1,1) end } },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = {},
+    },
+  },
+}

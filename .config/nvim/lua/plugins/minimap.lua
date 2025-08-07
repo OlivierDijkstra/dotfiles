eee77aff732bba84c0f6ff2d47a8f4e42c4cdb92
olivier dijkstra <olivierdijkstra@gmail.com>
2025-08-07 12:58:44 +0200

    fixes

--- .config/nvim/lua/plugins/minimap.lua
+++ .config/nvim/lua/plugins/minimap.lua
@@ -24,7 +24,49 @@ return {
       },
     })
 
+    -- Track minimap state
+    local minimap_was_open = false
+    
     -- Auto open minimap
-    vim.cmd("autocmd VimEnter * lua MiniMap.open()")
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        vim.defer_fn(function()
+          require("mini.map").open()
+        end, 100)
+      end,
+    })
+
+    -- Handle neo-tree toggle
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "neo-tree",
+      callback = function()
+        local minimap = require("mini.map")
+        -- Check if minimap is currently open
+        if minimap.current.win_data and minimap.current.win_data[1] then
+          minimap_was_open = true
+          minimap.close()
+        end
+      end,
+    })
+
+    -- Reopen minimap when leaving neo-tree
+    vim.api.nvim_create_autocmd("WinClosed", {
+      callback = function()
+        vim.defer_fn(function()
+          local neo_tree_open = false
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].filetype == "neo-tree" then
+              neo_tree_open = true
+              break
+            end
+          end
+          if not neo_tree_open and minimap_was_open then
+            require("mini.map").open()
+            minimap_was_open = false
+          end
+        end, 50)
+      end,
+    })
   end,
 }

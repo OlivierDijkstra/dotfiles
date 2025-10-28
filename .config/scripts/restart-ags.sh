d353645e75f5b0d89a7d692f644a8e334426464c
olivier dijkstra <olivierdijkstra@gmail.com>
2025-10-28 12:40:45 +0100

    clean

--- .config/scripts/restart-ags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Quick AGS restart script
-systemctl --user restart ags.service
-
-# Optional: send notification
-if command -v notify-send &> /dev/null; then
-    notify-send "AGS" "Bar restarted" -t 2000
-fi 
